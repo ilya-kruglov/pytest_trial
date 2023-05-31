684335755419eef4d6116d4d180a4d36345c5232
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-05-31 23:09:42 +0300

    Add Python debugger test

--- README.md
+++ README.md
@@ -20,3 +20,21 @@
 > pytest -k "correct"
 > pytest -k "not correct"
 > pytest -k "(django or python) and not javascript"
+
+#### Спринт 8/25 → Тема 5/8: Библиотека pytest → Урок 2/6
+
+> pytest test_example.py -qq --lf
+> pytest test_example.py -qq --ff
+> pytest --ff -x
+> pytest --ff --maxfail=2
+> pytest --sw
+> pytest --sw --stepwise-skip
+> pytest test_example.py -qq --nf
+
+#### Спринт 8/25 → Тема 5/8: Библиотека pytest → Урок 3/6
+
+> pytest -k "correct or fail"
+> pytest -s -k "correct or fail"
+> pytest --capture=no -k "correct or fail"
+> pytest -v -s -k "correct or fail"
+> pytest -rP -k "correct or fail"
--- test_example.py
+++ test_example.py
@@ -1,18 +1,9 @@
 # test_example.py
-import pytest
+# import pytest
 
 
 def one_more(x):
     return x + 1
-
-
-def test_correct():
-    assert one_more(4) == 5
-
-
-@pytest.mark.skip(reason="no way of currently testing this")
-def test_fail():
-    assert one_more(3) == 5
 
 
 def get_sort_list(string):
@@ -20,7 +11,26 @@ def get_sort_list(string):
     return new_list
 
 
+def test_correct():
+    print('Правильный тест')  # Новая строка.
+    assert one_more(4) == 5
+
+
+# @pytest.mark.skip(reason="no way of currently testing this")
+def test_fail():
+    print('Неправильный тест')  # Новая строка.
+    assert one_more(3) == 5
+
+
 def test_sort():
     """Тестируем функцию get_sort_list()."""
     result = get_sort_list('Яша, Саша, Маша, Даша')
     assert result == ['Даша', 'Маша', 'Саша', 'Яша']
+
+
+def test_type():
+    """Тестируем тип данных, возвращаемых из get_sort_list()."""
+    result = get_sort_list('Яша, Саша, Маша, Даша')
+    # Провальный тест:
+    # ожидаем число, но вернётся список.
+    assert isinstance(result, int)
